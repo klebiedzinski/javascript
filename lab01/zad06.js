d63ad8f921a597d14e987540b5449cb5d227ba29
Karol Lebiedziński <lebiedzinskik02@gmail.com>
2022-03-07 11:50:07 +0100

    koncowka lekcji

---
+++ lab01/zad06.js
@@ -0,0 +1,6 @@
+table = [5, true, "Ala", -2];
+for ( let i = 0 ; i < table.length ; ++i ){
+typ = "";
+
+
+}
---
+++ lab01/zmienne.js
@@ -0,0 +1,2 @@
+console.log('2'==2); //sprawdza wartosci ale nie sprawdza typu
+console.log('2'===2); //sprawdza czy typ jest taki sam
