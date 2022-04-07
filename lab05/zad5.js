2916e901261a3217bafe820d0f52f7b365ef828c
karol <lebiedzinskik02@gmail.com>
2022-04-07 18:06:16 +0200

    lab05 - dokonczenie

---
+++ lab05/zad5.js
@@ -0,0 +1,51 @@
+'use strict';
+const wishlist = [   
+    { name: 'Czajnik', netto: 100 },  
+    { name: 'Lodówka', netto: 2730 },  
+    { name: 'Mikrofalówka', netto: 940 },  
+    { name: 'Mikser', netto: 120 },  
+    { name: 'Piekarnik', netto: 3100 }, 
+    { name: 'Zmywarka', netto: 2400 },
+    { name: 'Toster', netto: 260 }
+  ]
+
+const totalPrice = wishlist.reduce((acum,el,curIndex,wishlist) => {
+    return acum+=wishlist[curIndex]['netto'];
+   
+},0);
+
+console.log(totalPrice);
+
+const toArr = wishlist.reduce((acum,el,curIndex,wishlist) => {
+    acum.push(wishlist[curIndex]['netto']);
+    return acum;
+   
+},[]);
+console.log(toArr);
+
+const newArr = wishlist.reduce((acum,el,curIndex,wishlist) => {
+    acum.push(wishlist[curIndex]['name'] + ':' + wishlist[curIndex]['netto']);
+    return acum
+},[]);
+
+console.log(newArr);
+
+const map = new Map(wishlist.entries(wishlist));
+console.log(map);
+
+
+const newArray = wishlist.reduce((elements, x,curIndex) => {
+    if (wishlist[curIndex]['netto'] < 500)  elements.push(wishlist[curIndex]['netto']);
+    return elements;
+},[]);
+
+console.log(newArray);
+
+const names = ['Ala','Alicja','Barbara']
+
+const findEl = names.reduce((maybeAla,curName) => {
+    if (maybeAla==='Ala') return maybeAla;
+    return maybeAla;
+});
+
+console.log(findEl);
