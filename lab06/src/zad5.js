546a40b85deb8075dcde95c7780c857d00e89798
Karol <lebiedzinskik02ni@gmail.com>
2022-04-25 16:08:24 +0200

    lab 06 - dom

--- lab06/src/zad5.js
+++ lab06/src/zad5.js
@@ -1,11 +1,16 @@
 'use strict';
 const zakupy = require('./listaZakupow.js').listaZakupow;
 
+
+//zadanie1
+
 // const result = zakupy.reduce((accum,curElement,curIndex) => {
 //     accum.push({[zakupy[curIndex]['produkt']] : curElement})
 //     return accum
 // },[]);
 // console.log(result)
+
+//zadanie2
 
 // const result = zakupy.reduce((accum,curElement,curIndex) => {
 //     if (zakupy[curIndex]['typ']==='nabiał')  accum+=(curElement['cena'])*(curElement['ilosc']) ;
@@ -13,14 +18,30 @@ const zakupy = require('./listaZakupow.js').listaZakupow;
 // },0);
 // console.log(result)
 
+// zadanie3
+
 // const result = zakupy.reduce((accum,curElement,curIndex) => {
 //     if (zakupy[curIndex]['jednostka']==='kg')  accum.push(curElement['produkt']) ;
 //     return accum.sort()
 // },[]);
 // console.log(result)
 
-const result = zakupy.reduce((accum,curElement,curIndex,parametr='nabiał') => {
-    if (zakupy[curIndex]['typ']===parametr)  accum.push(curElement['produkt']) ;
-    return accum.sort()
-},[]);
-console.log(result)
+//zadanie4
+
+// function ItemsByType(zakupy, type){
+//     const result = zakupy.reduce((accum, curEl) => {
+//         if(curEl['typ'] === type && curEl['cena'] * curEl['ilość'] < 10) accum.push(curEl["produkt"]);
+        
+//         return accum.sort();
+//     },[]);
+// }
+// console.log(ItemsByType(zakupy,"nabiał"));
+
+
+//zadanie5
+// const result = zakupy.reduce((accum,element,curIndex) => {
+//     if (zakupy[curIndex]['jednostka']==='sztuk')  accum.push(element['produkt']);
+//     return accum;
+// },[]);
+// console.log(result)
+ 
