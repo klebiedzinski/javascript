8ce6284c2a9d7c2698dd0ce3ff655741029dc6dd
Karol <lebiedzinskik02ni@gmail.com>
2022-05-05 19:40:46 +0200

    lab08 1/2

---
+++ lab08/src/creatingObjects.js
@@ -0,0 +1,167 @@
+// ========================================
+// ZADANIE 1
+// ========================================
+
+//  Zdefiniuj pola 'title' i 'author' i funkcję print(), która wypisze: author - title
+
+const book0 = {
+  title: "book0tittle",
+  author: "book0author",
+  print: function print(){
+    console.log(this.author,"-",this.title)
+  }
+};
+
+
+// book0.print();
+
+const book1 = {
+title: "book1tittle",
+author: "book1author",
+print: function print(){
+  console.log(this.author,"-",this.title)
+}
+}
+
+// book1.print();
+
+const book2 = Object.create(book1);
+book2.title = "book2tittle";
+book2.author = "book2author";
+
+
+// book2.print();
+
+function BookCreator(title, author) {
+  const b = {
+    title: title,
+    author: author,
+    print: function print(){
+      console.log(this.author,"-",this.title)
+    }
+  };
+  return b;
+}
+
+const book3 = BookCreator("Cień wiatru", "Carlos Ruiz Zafon");
+const book4 = BookCreator("Ojciech Chrzestny", "Mario Puzo");
+
+// book3.print();
+// book4.print();
+
+// ========================================
+// ZADANIE 2
+// ========================================
+
+// Przetestuj poniższy kod i odpowiedz na pytania
+
+function testThis() {
+  console.log(this);
+}
+
+// testThis();
+
+function testThis2() {
+  "use strict";
+  console.log(this);
+}
+
+// testThis2();
+
+// 2.1. Czym jest this? this to odnosnik to obiektu ktory zostal podany jako argument w funkcji
+// 2.2. Do czego odwołuje się this w obu przypadkach do obiektu podanego jako argument w funkcji, use strict 
+
+const person = {
+  name: "Oscar Wilde",
+  print(name = this.name) {
+    console.log(this.name);
+
+    function a() {
+      console.log("a: ", name);
+    }
+    a();
+  },
+};
+// person.print();
+
+// 2.3. Jakie wartości przyjmuje this w powyższych przypadkach i dlaczego?
+// 2.4. Zmodyfikuj powyższy kod w ten sposób, aby funkcja a wyświetlała w konsoli 'a: Oscar Wilde'. Nie używaj arrow function.
+
+const printName = function () {
+  console.log(this.name);
+};
+
+const person1 = {
+  name: "Aaron Towels",
+  print: printName
+};
+
+const person2 = {
+  name: "Tom Clancy",
+  print: printName
+};
+// 2.5. Za pomocą funkcji printName wypisz 'name' obu autorów. Nie zmieniaj implementacji funkcji printName!
+
+// person1.print()
+// person2.print()
+
+
+
+
+const person3 = {
+  name: "Arthur Conan Doyle",
+  print() {
+    const a = () => {
+      console.log(this);
+    };
+    a();
+  },
+};
+// person3.print();
+
+// 2.6. Co wydrukuje w konsoli powyższy kod? Jaki scope ma arrow function? wiekszy od 'normalnie' zadeklarowanej funkcji
+
+// ========================================
+// ZADANIE 3
+// ========================================
+
+// Powróćmy do zadania 1.
+// Dlaczego nasza funkcja BookCreator nie jest najlepszym rozwiązaniem do tworzenia obiektów? poniewaz jej scope jest zbyt "plytki"
+
+// Zmodyfikuj funkcję BookCreator tak, aby inicjalizowała pola author i title.
+function BetterBookCreator(title, author) {
+  const b = {
+    title: title,
+    author: author,
+    print: function print(){
+      console.log(this.author,"-",this.title)
+    },
+    readers: 0,
+    addReader: function foo(el) {this.readers+=el}
+  };
+  return b;
+}
+const book5 = BetterBookCreator("Lalka", "Bolesław Prus")
+// book5.print()
+book5.addReader(1)
+book5.addReader(1)
+// console.log(book5.readers)
+book5.hasOwnPropety('isBestseller')
+
+// Funkcję print zadeklaruj jako wspólną dla wszystkich obiektów tworzonych przez BookCreator.
+// Dopisz do tworzonych obiektów pole readers, które będzie zawierało liczbę czytelników.
+// Zadeklaruj funkcję addReader, która inkrementuje pole readers. addReader powinna być funkcją wspólną, tak jak print.
+
+// ========================================
+// * ZADANIE 4
+// ========================================
+
+// Na stworzonym obiekcie wywołaj funkcję hasOwnProperty('isBestseller').
+// ========================================
+// Napisz dlaczego nasz obiekt ma do niej dostęp. (jeśli wyskakuje błąd - powróć do poprzedniego zadania)
+
+// ========================================
+// * ZADANIE 5
+// ========================================
+
+// Odwołaj się do zmiennej __proto__ w stworzonym obiekcie, co zawiera ta zmienna i do czego służy?
---
+++ lab08/src/objects.js
@@ -0,0 +1,120 @@
+﻿"use strict";
+
+// =======================
+// Przeanalizuj poniższe fragmenty kodu i zastanów się, co zostanie wypisane na ekranie.
+
+// 1
+
+const arr = ["Pies", "Kot", "Królik", "Papuga", "Małpa"];
+const [pies, kot, krolik, papuga, malpa] = arr;
+
+// console.log(pies, krolik);
+
+// 2
+
+const book1 = {
+  title1: "Mistrz i Małgorzata",
+  author1: "Michaił Bułhakow",
+};
+
+const { 
+  title1 = "-", 
+  author1 = "-", 
+  year1 = 1967 
+} = book1;
+
+// console.log(title1);
+// console.log(author1);
+// console.log(year1);
+
+// 3
+
+const book2 = {
+  titleOfBook: "Mistrz i Małgorzata",
+  authorOfBook: "Michaił Bułhakow",
+  year2: 1967,
+};
+
+const {
+  titleOfBook: title2 = "-",
+  authorOfBook: author2 = "-",
+  year2 = "-",
+} = book2;
+
+// console.log(title2);
+// console.log(author2);
+// console.log(year2);
+
+// 4
+
+const book3 = {
+  title3: "Mistrz i Małgorzata",
+  author3: "Michaił Bułhakow",
+  year3: 1967,
+};
+
+const { title3, ...bookWithoutTitle } = book3;
+
+// console.log(title3);
+// console.log(bookWithoutTitle);
+
+// ==============================================
+
+// 5
+// Wykorzystując wszystkie informacje zawarte powyżej stwórz obiekt
+// zawierający poniższe pola:
+// Country: USA
+// Title: Zielona Mila
+// Director: Frank Darabont
+// Year: 1999
+// Genre: Dramat
+
+const movie1 = {
+  Country: "USA",
+  Title: "Zielona Mila",
+  Director: "Frank Darabont",
+  Year: 1999,
+  Genre: "Dramat"
+};
+
+// Następnie wykorzystując zabieg przedstawiony w powyższych przykładach zmodyfikuj obiekt tak, aby po wyświetleniu obiektu dostać następujący output:
+
+const {
+  Country,
+  Year,
+  Genre,
+  ...newMovie1
+  
+} = movie1;
+
+// console.log(newMovie1);
+// Oczekiwany output: { title: 'Zielona Mila', director: 'Frank Darabont' }
+
+// 6
+// Stwórz teraz bardziej skomplikowany obiekt zawierający m.in tablicę i inny obiekt. Powiedzmy, że zawiera np.
+// Title: Nietykalni
+// Directors: Olivier Nakache, Éric Toledano
+// Country: Francja
+// Year: 2011
+// Actors: Philippe: François Cluzet, Dris: Omar Sy, Yvonne: Anne Le Ny, ...
+const movie2 = {
+  Title: "Nietykalni",
+  Directors: ["Olivier Nakache", "Éric Toledano"],
+  Country: "Francja",
+  Year: 2011,
+  Actors: {Philippe: "François Cluzet", Dris:" Omar Sy", Yvonne: "Anne Le Ny"}
+};
+
+// Następnie odwzoruj wygląd stworzonej struktury obiektu i wykonaj na nim poniższe operacje
+
+const {
+  title = movie2.Title,
+  director1 = movie2.Directors[0],
+  DrisRole = movie2.Actors.Dris
+} = movie2;
+
+console.log(title); // Oczekiwany output -> Nietykalni
+console.log(director1); // Oczekiwany output -> Olivier Nakache
+console.log(DrisRole); // Oczekiwany output -> Omar Sy
+
+// Zmodyfikuj teraz swój kod, aby zawierał domyślne wartości, jeśli którakolwiek z własności filmu nie została podana.
