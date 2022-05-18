cd64e4b45678197c27f44f98441e4b53436a0268
Karol <lebiedzinskik02ni@gmail.com>
2022-05-18 23:40:24 +0200

    kolokwium

--- lab08/src/reduce.js
+++ lab08/src/reduce.js
@@ -2,3 +2,7 @@ const arr = [
     { key: 0, x: [4, 5, [6]], y: [1, 2, [3, [4]]] },
     { key: 0, x: [1], y: [] }
 ];
+const zad =arr.reduce((akum,el) =>{
+    akum= akum+ [...el.x].length + [...el.x].length
+    return akum
+},0)
