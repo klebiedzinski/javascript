5a8872d178c85193551230f829ffc5d8e415457c
Karol <lebiedzinskik02ni@gmail.com>
2022-06-02 08:56:05 +0200

    promisy, zadania do kartkowki

--- lab10-Kolokwium/KOLOKWIUM/kolokwiumWDomu/zad1.js
+++ lab10-Kolokwium/KOLOKWIUM/kolokwiumWDomu/zad1.js
@@ -1,18 +1,1 @@
 const { series } = require("./series");
-
-const miniseries = series.reduce ((akum,el) => {
-    if (el.seasons===1){
-        const a = {
-            name: el.name,
-            year: el.startYear,
-            type: el.type
-        }
-        akum.push(a)
-    }
-    return akum
-
-})
-
-
-console.log(miniseries)
-
